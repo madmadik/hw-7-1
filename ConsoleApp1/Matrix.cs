f53b87541a43299f35a289158b647ebe53f2696d
madik <madik404error@gmail.com>
2017-12-04 22:11:01 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Matrix.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Matrix
+    {
+        int size;
+        double[] matrix;
+
+        public void SetSize()
+        {
+            bool check = int.TryParse(Console.ReadLine(), out size);
+
+            if(check)
+            {
+                matrix = new double[size];
+            }
+            else
+            {
+                matrix = null;
+                Console.WriteLine("not correctly parsing!");
+            }
+        }
+
+        public int GetSize()
+        {
+            return size;
+        }
+
+        public double this[int index]
+        {
+            get
+            {
+                return matrix[index];
+            }
+            set
+            {
+                int degree = 2;
+                matrix[index] = Math.Pow(value, degree);
+            }
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix matrix = new Matrix();
+            matrix.SetSize();
+            for(int i=0; i<matrix.GetSize(); i++)
+            {
+                matrix[i] = i;
+            }
+            for(int i=0;i<matrix.GetSize();i++)
+            {
+                Console.WriteLine(matrix[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
